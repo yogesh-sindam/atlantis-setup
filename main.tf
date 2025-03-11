793e0d4aaede731ec5efe902a09e62b0718c0d6e
Yogesh.sindam <91203370+yogesh-sindam@users.noreply.github.com>
2025-03-11 22:04:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,40 @@
+
+############ PROVIDER BLOCK ############
+provider "aws" {
+  region     = "us-east-1"
+  profile    = "default"
+}
+############ SAVING TF STATE FILE #########
+terraform {
+  backend "s3" {
+    bucket  = "test-atlantis-easydeploy"
+    key     = "atlantis/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "default"
+  }
+}
+################# EC2 INSTANCE CREATION #########
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-atlantis"
+  }
+}
